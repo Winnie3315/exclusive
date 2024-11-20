9bafc0a6e7621ac34641bcf4c2ada7c61e6bd0e2
Winnie3315 <winniiemae777@gmail.com>
2024-11-20 13:04:19 +0500

    trying to solve a problem

--- db.ts
+++ db.ts
@@ -1,33 +1,38 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose, { Mongoose } from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URL!;
 
 interface MongooseConn {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null; 
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
 let cached: MongooseConn = (global as any).mongoose;
 
 if (!cached) {
-    cached = (global as any).mongoose = {
-        conn: null,
-        promise: null,
-    };
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
 }
 
 export const connect = async () => {
-    if (cached.conn) return cached.conn;
-
-    cached.promise =
-        cached.promise ||
-        mongoose.connect(MONGODB_URL, {
-            dbName: "exclusive",
-            bufferCommands: false,
-            connectTimeoutMS: 300000,
-        });
-
-    cached.conn = await cached.promise;
-
+  if (cached.conn) {
+    console.log("Already connected to MongoDB");
     return cached.conn;
+  }
+
+  console.log("Connecting to MongoDB...");
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: "exclusive",
+      bufferCommands: false,
+      connectTimeoutMS: 30000,
+    });
+
+  cached.conn = await cached.promise;
+  console.log("Connected to MongoDB");
+
+  return cached.conn;
 };
